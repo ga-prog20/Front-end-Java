f11041aabb69d7cda5d228ae6d59787eed0de601
ga-prog20 <61214205+ga-prog20@users.noreply.github.com>
2020-03-26 22:53:28 -0300

    Add files via upload

---
+++ Matricula.java
@@ -0,0 +1,62 @@
+        public class Matricula {
+
+        private Aluno aluno;
+
+        private Curso codigoDoCurso;
+
+        private Data date;
+
+
+
+        public Aluno getAluno() {
+
+            return aluno;
+
+        }
+
+
+
+        public void setAluno(Aluno aluno) {
+
+            this.aluno = aluno;
+
+        }
+
+
+
+        public Curso getCodigoDoCurso() {
+
+            return codigoDoCurso;
+
+        }
+
+
+
+        public void setCodigoDoCurso(Curso codigoDoCurso) {
+
+            this.codigoDoCurso = codigoDoCurso;
+
+        }
+
+
+
+        public Data getData() {
+
+            return date;
+
+        }
+
+
+
+        public void setData(Data data) {
+
+            this.data = data;
+
+        }
+
+    }
+
+
+
+
+}
